b0d428fdc45dd608dde12b4f16185c536886e18e
Maharsh04 <r.maharsh@iitg.ac.in>
2023-07-04 21:42:21 +0530

    Additional commit

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('https://convoconnect.onrender.com/join', (req, res) => {
     res.render('join');
 });
 
-app.get('https://convoconnect.onrender.com/', (req, res) => {
+app.get('/', (req, res) => {
     res.render('home');
 });
 
